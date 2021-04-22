c690cf0a390423ff78c014770415711ea41a0733
unknown <muvvala.nagasiva1995@gmail.com>
2021-04-22 12:25:05 +0530

    washing

---
+++ src/main/java/flipkart5/1/action/HelloWorldAction.java
@@ -0,0 +1,32 @@
+package flipkart5.1.action;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import pl.bristleback.server.bristle.api.action.DefaultAction;
+import pl.bristleback.server.bristle.api.annotations.Action;
+import pl.bristleback.server.bristle.api.annotations.ActionClass;
+import pl.bristleback.server.bristle.engine.user.BaseUserContext;
+import pl.bristleback.server.bristle.serialization.system.annotation.Bind;
+
+@ActionClass(name = "HelloWorld")
+@Controller
+public class HelloWorldAction implements DefaultAction<BaseUserContext, String> {
+
+  @Autowired
+  private SampleClientAction sampleClientAction;
+
+  @Action
+  public String executeDefault(BaseUserContext defaultUser, @Bind(required = true) String text) {
+    return new StringBuilder(text).reverse().toString();
+  }
+
+  @Action
+  public String duplicateText(@Bind(required = true) String text) {
+    return text + text;
+  }
+
+  @Action
+  public void sayHelloToAll() {
+    sampleClientAction.sayHello("Hello everyone!");
+  }
+}
